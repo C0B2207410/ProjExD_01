7eb21414536d67b9f1dec7cd2a949a10ac4d3de4
C0B22074 <c0b2207410@edu.teu.ac.jp>
2023-06-13 06:37:15 +0000

    lec5

--- pygame_image.py
+++ pygame_image.py
@@ -7,15 +7,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    koukaton_img = pg.image.load("ex01/fig/3.jpg")
+    koukaton_img = pg.image.load("ex01/fig/3.png")
     koukaton_img = pg.transform.flip(koukaton_img, True, False)
-    koukaton_imgs = [koukaton_img, pg.transform.rotozoom(koukaton_img, 10)]
+    koukaton_imgs = [koukaton_img, pg.transform.rotozoom(koukaton_img, 10, 1)]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(koukaton_imgs[tmr%2], [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
